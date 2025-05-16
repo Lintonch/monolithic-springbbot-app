28bd03ce4bb589c491aec03d34adce78425873ae
Lintonch <lintonchoudhury1993@gmail.com>
2025-05-16 05:58:21 +0530

    first commit

---
+++ src/main/java/com/ffma/FullFeaturedMonolithicAppApplication.java
@@ -0,0 +1,13 @@
+package com.ffma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullFeaturedMonolithicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullFeaturedMonolithicAppApplication.class, args);
+	}
+
+}
